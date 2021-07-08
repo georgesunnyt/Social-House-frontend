eb2a43b833e346f0fca479e098e8b143a895c9c3
George Sunny <georgesunnyt@gmail.com>
2021-07-08 06:17:40 +0530

    added account page

---
+++ src/features/account/Account.js
@@ -0,0 +1,51 @@
+import './Account.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import PostList from '../posts/PostsList/PostsList'
+import { useEffect, useRef, useState } from 'react'
+
+export default function Account() {
+
+    const [coverPicYpos, setCoverPicYpos] =useState(0);
+    const [profileOpacity, setProfileOpacity] =useState(1)
+
+    const handleScroll = () => {
+        console.log(document.documentElement.scrollTop)
+        console.log(window.screen.width)
+        let x = document.documentElement.scrollTop;
+        if(window.screen.width <= 600) {
+            setCoverPicYpos(x/1.1)
+            setProfileOpacity((90-x/2))
+        } else {
+            setCoverPicYpos(x/3)
+            setProfileOpacity((90-x/4))
+        }
+    }
+
+    useEffect(()=> {
+        window.addEventListener('scroll',handleScroll)
+        return ()=> {
+            window.removeEventListener('scroll',handleScroll)
+        }
+    },[])
+
+    return(
+        <>
+            <div className='account-main-container'>
+                <img className='cover-pic' style={{objectPosition: `50% ${coverPicYpos}%`}} src='https://images8.alphacoders.com/885/885522.jpg' alt=''></img>
+                <div className='account-card-flex-container' style={{transform: `perspective(200px) translateZ(-${coverPicYpos}px)`, opacity:`${profileOpacity}`}} >
+                    <img className='profile-pic' src='https://i.imgur.com/b0hZGg4.png' alt=''></img>
+                    <div className='card-details'>
+                        <h1>Polly Gray</h1>
+                        <h3>Accountant at Shelby Private Limited</h3>
+                        <div>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon>
+                            <span> Birmingham</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <PostList user={'Polly Gray (you)'}></PostList>
+        </>
+    )
+}
--- src/features/navBar/NavBar.js
+++ src/features/navBar/NavBar.js
@@ -1,7 +1,7 @@
 import './NavBar.css'
 import {NavLink} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUsers, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUsers, faArrowAltCircleRight, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar(props) {
     return(
@@ -11,13 +11,17 @@ export default function NavBar(props) {
                         <span className='logo'>Social House</span>
                         <FontAwesomeIcon icon={faArrowAltCircleRight}></FontAwesomeIcon>
                     </div>
-                    <NavLink exact to='/posts' activeClassName="selectedRoute">
+                    <NavLink exact to='/home' activeClassName="selectedRoute">
                         <FontAwesomeIcon icon={faHome} ></FontAwesomeIcon>
                         <span>View posts</span>
                     </NavLink>
                     <NavLink exact to='/users' activeClassName="selectedRoute">
                         <FontAwesomeIcon icon={faUsers} ></FontAwesomeIcon>
                         <span>People</span>
+                    </NavLink>
+                    <NavLink exact to='/account' activeClassName="selectedRoute">
+                        <FontAwesomeIcon icon={faUserCircle} ></FontAwesomeIcon>
+                        <span>Account</span>
                     </NavLink>
             </div>
         </>
