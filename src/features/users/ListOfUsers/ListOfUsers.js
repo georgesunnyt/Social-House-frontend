08cc8b55a30f498c2d0d16f815c2d6611fcddb20
georgesunnyt <georgesunnyt@gmail.com>
2021-07-18 05:07:13 +0530

    added loading animation to people page

--- src/features/users/ListOfUsers/ListOfUsers.js
+++ src/features/users/ListOfUsers/ListOfUsers.js
@@ -1,16 +1,26 @@
 import { useGetUsersByPageQuery } from "../usersSlice";
 
 export default function ListOfUsers(props) {
-    const {data} = useGetUsersByPageQuery(props.pageNo, {pollingInerval:0});
+    const {data, isFetching} = useGetUsersByPageQuery(props.pageNo, {pollingInterval:0});
+    let loadingData = []
+    for(let i=0; i<6; i++) {
+        loadingData.push({id:i,name:'loading'})
+    }
+
     return(
      <>
            <h2 className='users-title-pc-view'>{props.title}</h2>
-           {data?.data.map((user)=>(
+           {!isFetching? data?.data.map((user)=>(
                 <div key={user.id} className='single-comment'>
                     <img src={user.avatar} className='dp' alt=''></img>
                     <div>{user.first_name} {user.last_name}</div>
                 </div>
-            ))}
+            )): loadingData.map((user)=>(
+                <div key={user.id} style={{height:'5vh', minWidth:'15vw'}}className='single-comment loading'>
+                    <div>{user.name}</div>
+                </div>
+            ))
+            }
      </>
     )
 }
