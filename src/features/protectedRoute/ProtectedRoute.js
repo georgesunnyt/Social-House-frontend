b587ea1fe76c6561ad0dc1a4175f06dbda7a0342
georgesunnyt <georgesunnyt@gmail.com>
2021-07-19 19:15:53 +0530

    removed console logs

--- src/features/protectedRoute/ProtectedRoute.js
+++ src/features/protectedRoute/ProtectedRoute.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 
 export default function ProtectedRoute({component: Component,computedMatch,...rest}) {
     const authenticated = useSelector(state=>state.login.isAuthenticated)
-    console.log('inside protected component and ' + authenticated )
     return authenticated? 
     (<Route {...rest}>
         <Component match={computedMatch}></Component>
