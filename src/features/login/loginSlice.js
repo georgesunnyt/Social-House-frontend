5ca31a5ac0bf8bb16ae0ceba431de9ee91d9909d
georgesunnyt <georgesunnyt@gmail.com>
2021-07-22 20:18:24 +0530

    enhanced login experience to stay logged in unless logged out and fixed login bypass bug on refresh

--- src/features/login/loginSlice.js
+++ src/features/login/loginSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isAuthenticated: sessionStorage.getItem('isAuthenticated'),
-    username: ''
+    isAuthenticated: localStorage.getItem('isAuthenticated'),
+    username: localStorage.getItem('username')
 }
 
 const loginSlice = createSlice({
@@ -11,13 +11,15 @@ const loginSlice = createSlice({
     reducers: {
         authenticate(state, action) {
             state.isAuthenticated = true
-            sessionStorage.setItem('isAuthenticated', true)
             state.username = action.payload.username
+            localStorage.setItem('isAuthenticated', true)
+            localStorage.setItem('username', action.payload.username)
         },
         logout(state) {
             state.isAuthenticated = false
             state.username = ''
-            sessionStorage.setItem('isAuthenticated', false)
+            localStorage.removeItem('isAuthenticated')
+            localStorage.removeItem('username')
         }
     }
 })
